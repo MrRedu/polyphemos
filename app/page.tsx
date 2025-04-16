6a6af18c2d0ac1df5a9ae44a0d8baae36523918d
Eduardo <ejrb1234@gmail.com>
2025-04-16 17:23:49 -0400

    chore: Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <Button className="cursor-pointer">Épale</Button>
+    </>
+  );
+}

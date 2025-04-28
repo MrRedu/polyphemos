cf63c7a0e40b901ad7b2e8595b048c65c2ae115b
Eduardo <ejrb1234@gmail.com>
2025-04-28 11:09:25 -0400

    feat(i18n): add next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,53 @@
+import './globals.css'
+
+import { type Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { notFound } from 'next/navigation'
+import { hasLocale, NextIntlClientProvider } from 'next-intl'
+
+import { routing } from '@/i18n/routing'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Polyphemos',
+    default: 'Polyphemos',
+  },
+  authors: [{ name: 'Eduardo', url: 'https://github.com/MrRedu' }],
+  creator: 'Mr Redu',
+  description: '',
+  keywords: ['Blog', '', ''],
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  return (
+    <html lang={locale}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}

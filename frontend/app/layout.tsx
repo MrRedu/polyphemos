0a5c43f2ae17576f8b750cf498f16aaea36b857c
Eduardo <ejrb1234@gmail.com>
2025-12-22 21:38:03 -0400

    new font, add background styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
-import { Cormorant, Raleway } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/components/providers';
 
-const cormorant = Cormorant({
-  variable: '--font-cormorant',
-  subsets: ['latin'],
-});
-
-const raleway = Raleway({
-  variable: '--font-raleway',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
@@ -20,21 +15,7 @@ export const metadata: Metadata = {
   },
   description:
     'Polyphemos es tu fuente definitiva para todo lo relacionado con la programación y el desarrollo web. En Polyphemos, nos apasiona explorar y aprender sobre el desarrollo de productos digitales innovadores, especialmente en el ámbito web.',
-  keywords: [
-    'desarrollo web',
-    'programación web',
-    'front-end development',
-    'productos digitales',
-    'desarrollo web innovador',
-    'tutoriales desarrollo web front-end',
-    'programación productos digitales web',
-    'explorar programación web Polyphemos',
-    'guías front-end productos innovadores',
-    'Polyphemos desarrollo web',
-    'innovación web front-end',
-    'crear productos digitales web',
-    'pasión desarrollo web',
-  ],
+  keywords: ['desarrollo web', 'programación web', 'front-end'],
   authors: [{ name: 'Eduardo', url: 'https://github.com/MrRedu' }],
   creator: 'Eduardo',
   category: 'technology',
@@ -47,7 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${cormorant.variable} ${raleway.variable} antialiased`}>
+      <body className={`${inter.variable} antialiased bg-[#f7f7f7]`}>
+        {/* Background dots */}
+        <div className="fixed inset-0 bg-[radial-gradient(circle,#73737350_1px,transparent_1px)] bg-[size:16px_16px] -z-10" />
         <Providers>{children}</Providers>
       </body>
     </html>
